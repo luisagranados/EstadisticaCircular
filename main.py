4ac407caaadafcde466f826b8c58d8fc112ef459
Uzmar Gomez <uzmar.gomez@hotmail.com>
2023-02-22 22:20:08 +0000

    enter article data for plot

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import numpy as np
-from astropy.stats import circmean
-from astropy import units as u
-data = np.array([51, 67, 40, 109, 31, 358])*u.deg
-print(circmean(data)) 
+import pandas as pd
+import calendar
+
+month_names=[]
+for x in range(1,13):
+    month_names.append(calendar.month_abbr[x])
+
+month_angles=[]
+for x in range(0,12):
+    month_angles.append(15+30*x)
+
+data = [21, 23, 32, 31, 29, 20, 19, 18, 15, 17, 21, 16]
+
+df = pd.DataFrame(list(zip(month_names,month_angles,data)),columns = ['month','month_angle','data'])
+print(df)
