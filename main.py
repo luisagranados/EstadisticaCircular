1c2b52192f28a25df55c0ca097aef78ab873ee71
Uzmar Gomez <uzmar.gomez@hotmail.com>
2023-02-22 22:31:44 +0000

    plot function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import calendar
+import numpy as np
+import matplotlib.pyplot as plt
 
 month_names=[]
 for x in range(1,13):
@@ -11,5 +13,12 @@ for x in range(0,12):
 
 data = [21, 23, 32, 31, 29, 20, 19, 18, 15, 17, 21, 16]
 
-df = pd.DataFrame(list(zip(month_names,month_angles,data)),columns = ['month','month_angle','data'])
-print(df)
+# df = pd.DataFrame(list(zip(month_names,month_angles,data)),columns = ['month','month_angle','data'])
+# print(df)
+
+width = np.pi / 4 * .5
+
+ax = plt.subplot(projection='polar')
+ax.bar(month_angles, data, width=width, bottom=0.0, alpha=0.5)
+
+plt.savefig('test.png')
